12ccf3812bbd342240d1c45f61101332421c4ea0
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2021-08-23 16:58:35 -0700

    Updated build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 mkdir -p build && cd build
 
 # Configure
-cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Coverage ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
-cmake --build . --config Debug -- -j $(nproc)
+cmake --build . -- -j $(nproc)
 # Test
 ctest -j $(nproc) --output-on-failure
