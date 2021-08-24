2600f4d5dbaa37c4d1de4c8697ee9541b6d2336f
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2021-08-23 17:10:26 -0700

    Updated build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ cmake -DCMAKE_BUILD_TYPE=Coverage ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . -- -j $(nproc)
 # Test
-ctest -j $(nproc) --output-on-failure
+./tests/transfer_function_tests
